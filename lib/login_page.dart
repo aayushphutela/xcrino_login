6a2ff6f74656b089e4e8c07892f610efafc59d60
aayushphutela <125531877+aayushphutela@users.noreply.github.com>
2023-02-22 15:23:32 +0530

    huihuihuhui

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:xcrino_login/list_screen.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -9,6 +10,9 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController emailController = new TextEditingController();
+  final TextEditingController passwordController = new TextEditingController();
   @override
   Widget build(BuildContext context) {
     final deviceHieght = MediaQuery.of(context).size.height;
@@ -21,125 +25,127 @@ class _LoginPageState extends State<LoginPage> {
               Flexible(
                 child: Container(
                   decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(bottomLeft: Radius.circular(40),bottomRight:Radius.circular(40) )),
+                      color: Colors.amber,
+                      image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage(
+                            'assets/images/xcrino.png',
+                          )),
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(40),
+                          bottomRight: Radius.circular(40))),
                   height: 400,
-                  child: Image.asset(
-                    'assets/images/xcrino.png',
-                    fit: BoxFit.contain,
-                  ),
                 ),
               ),
               SizedBox(
                 height: 30.0,
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Align(
-                    alignment: Alignment.topLeft,
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 40, top: 15),
-                      child: Text(
-                        "Login",
-                        style: GoogleFonts.akshar(
-                            textStyle: TextStyle(
-                          color: Colors.deepPurpleAccent,
-                          fontSize: 45,
-                          fontWeight: FontWeight.bold,
-                        )),
-                        textAlign: TextAlign.start,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(22.0),
-                    child: Column(
-                      children: [
-                        TextFormField(
-                          decoration: InputDecoration(
-                              contentPadding:
-                                  EdgeInsets.fromLTRB(30, 15, 20, 15),
-                              hintText: "Username",
-                              border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(22))),
-                          autofocus: false,
-
-                          keyboardType: TextInputType.name,
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return ("Username Is Required");
-                            }
-                          },
-                          // onSaved: (value) {
-                          //   collegeEditingcontroller.text = value!;
-                          // },
-                          // textInputAction: TextInputAction.next,
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 22, right: 22),
-                    child: Column(
-                      children: [
-                        TextFormField(
-                          decoration: InputDecoration(
-                              contentPadding:
-                                  EdgeInsets.fromLTRB(30, 15, 20, 15),
-                              hintText: "Password",
-                              border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(22))),
-                          autofocus: false,
-
-                          keyboardType: TextInputType.name,
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return ("Password Is Required");
-                            }
-                          },
-                          // onSaved: (value) {
-                          //   collegeEditingcontroller.text = value!;
-                          // },
-                          // textInputAction: TextInputAction.next,
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(22.0),
-                    child: Material(
-                      elevation: 5,
-                      borderRadius: BorderRadius.circular(22),
-                      color: Colors.deepPurpleAccent,
-                      child: MaterialButton(
-                        onPressed: () {
-                          // signIn(emailController.text, passwordController.text);
-                        },
-                        padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
-                        minWidth: MediaQuery.of(context).size.width,
+              Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 40, top: 15),
                         child: Text(
-                          'Sign In',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              fontSize: 20,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold),
+                          "Login",
+                          style: GoogleFonts.akshar(
+                              textStyle: TextStyle(
+                            color: Colors.deepPurpleAccent,
+                            fontSize: 45,
+                            fontWeight: FontWeight.bold,
+                          )),
+                          textAlign: TextAlign.start,
                         ),
                       ),
                     ),
-                  ),
-                  // Container(
-                  //   alignment: Alignment.bottomCenter,
-                  //   height:140 ,
-                  //   child: Image.asset(
-                  //     'assets/images/line.png',
-                  //   ),
-                  // ),
-                ],
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(22.0),
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            decoration: InputDecoration(
+                                contentPadding:
+                                    EdgeInsets.fromLTRB(30, 15, 20, 15),
+                                hintText: "Username",
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(22))),
+                            autofocus: false,
+
+                            keyboardType: TextInputType.name,
+                            validator: (value) {
+                              if (value!= 'AVI1052') {
+                                return ("Invalid Username");
+                              }
+
+                            },
+                            onSaved: (value) {
+                              emailController.text = value!;
+                            },
+                            textInputAction: TextInputAction.next,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 22, right: 22),
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            obscureText: true,
+                            decoration: InputDecoration(
+                                contentPadding:
+                                    EdgeInsets.fromLTRB(30, 15, 20, 15),
+                                hintText: "Password",
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(22))),
+                            autofocus: false,
+
+                            validator: (value) {
+                              if (value!= '1122') {
+                                return ("Invalid Password");
+                              }
+
+                            },
+                            onSaved: (value) {
+                              passwordController.text = value!;
+                            },
+                            textInputAction: TextInputAction.next,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(22.0),
+                      child: Material(
+                        elevation: 5,
+                        borderRadius: BorderRadius.circular(22),
+                        color: Colors.deepPurpleAccent,
+                        child: MaterialButton(
+                          onPressed: () {
+                            signIn(emailController.text, passwordController.text);
+                          },
+                          padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+                          minWidth: MediaQuery.of(context).size.width,
+                          child: Text(
+                            'Sign In',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
@@ -147,4 +153,10 @@ class _LoginPageState extends State<LoginPage> {
       ),
     );
   }
-}
+  void signIn(String email, String password) async {
+    if (_formKey.currentState!.validate()) {
+
+
+       Navigator.push(context, MaterialPageRoute(builder: (context)=> ListScreen()));
+      }
+}}
